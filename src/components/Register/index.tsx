a37db84f48073a3e611855f76dc65edea44bbd45
patrick <physcript@gmail.com>
2022-01-15 23:37:02 +0800

    register url fixed

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -24,7 +24,7 @@ const Register: React.FunctionComponent<IRegister> = props => {
 
     const registerHandler = () => {
         setLoading(true)
-        const url = 'http://localhost:1337/api/create'
+        const url = 'https://blog-ts.herokuapp.com/create'
         const myData = {
             "name": regData.name,
             "email": regData.email,
